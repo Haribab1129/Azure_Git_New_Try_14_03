66d417fb6c412ff237b52bbc28a21b253fe5678b
Haribab1129 <hariarigela70956@gmail.com>
2023-03-14 17:52:50 +0530

    new commit

---
+++ src/main/java/com/mycompany/azure_new_process_14_03/Azure_New_Process_14_03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.azure_new_process_14_03;
+
+/**
+ *
+ * @author ICX
+ */
+public class Azure_New_Process_14_03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
